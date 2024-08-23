66fc0c065199f1f5dac6926cbe6b7d80f30a4307
zerog975 <114834752+zerog975@users.noreply.github.com>
2024-08-23 02:45:24 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybitcointools',
+    version='1.1.15',
+    packages=find_packages(),
+    description='Python Bitcoin Tools',
+    author='Vitalik Buterin',
+    url='https://github.com/vbuterin/pybitcointools',
+    install_requires=[],
+)
